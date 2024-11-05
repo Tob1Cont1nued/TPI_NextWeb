c97f9f144e1d88273179ca92f1f2736cbb00a938
KolmThomas <Thomas.Kolm@outlook.com>
2024-11-05 10:03:02 +0100

    Organisiert

--- tpi_next_web/src/app/app.routes.ts
+++ tpi_next_web/src/app/app.routes.ts
@@ -5,17 +5,17 @@ import { ParticipationComponent } from './pages/2_participation/participation.co
 import { TeststrategyComponent } from './pages/3_teststrategy/teststrategy.component';
 import { TestorganisationComponent } from './pages/4_testorganisation/testorganisation.component';
 import { CommunicationComponent } from './pages/5_communication/communication.component';
+import { TestprocessmanagementComponent } from './pages/7_testprocessmanagement/testprocessmanagement.component';
 import { ReportingComponent } from './pages/6_reporting/reporting.component';
-import { PlanningComponent } from './pages/7_planning/planning.component';
-import { MetricsComponent } from './pages/8_metrics/metrics.component';
-import { DefectmanagementComponent } from './pages/9_defectmanagement/defectmanagement.component';
-import { TestprocessmanagementComponent } from './pages/10_testprocessmanagement/testprocessmanagement.component';
-import { MethodicalApproachComponent } from './pages/11_methodical-approach/methodical-approach.component';
-import { ProfessionalismComponent } from './pages/12_professionalism/professionalism.component';
-import { TestdesignComponent } from './pages/13_testdesign/testdesign.component';
-import { TestwaremanagementComponent } from './pages/14_testwaremanagement/testwaremanagement.component';
-import { EnvironmentComponent } from './pages/15_environment/environment.component';
-import { ToolsComponent } from './pages/16_tools/tools.component';
+import { PlanningComponent } from './pages/8_planning/planning.component';
+import { MetricsComponent } from './pages/9_metrics/metrics.component';
+import { DefectmanagementComponent } from './pages/10_defectmanagement/defectmanagement.component';
+import { TestwaremanagementComponent } from './pages/11_testwaremanagement/testwaremanagement.component';
+import { MethodicalApproachComponent } from './pages/12_methodical-approach/methodical-approach.component';
+import { ProfessionalismComponent } from './pages/13_professionalism/professionalism.component';
+import { TestdesignComponent } from './pages/14_testdesign/testdesign.component';
+import { ToolsComponent } from './pages/15_tools/tools.component';
+import { EnvironmentComponent } from './pages/16_environment/environment.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { LoginComponent } from './pages/login/login.component.spec';
 
